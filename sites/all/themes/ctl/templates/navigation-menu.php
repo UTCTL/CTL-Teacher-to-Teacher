aa798281d5d4477f56aedc48803178cf37c52698
Julia Lou <julialou896@gmail.com>
2012-08-31 10:24:33 -0500

    august 31

---
+++ sites/all/themes/ctl/templates/navigation-menu.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @file
+ * Navigation menu
+*/
+
+?> 
+ 
+ <nav id="navigation-menu" role="navigation">
+      <ul id="nav-menu">
+        <li id="nav-start">
+          <a href="<?php print $front_page; ?>" title="Start">Start</a>
+        </li>
+        <li class="nav-modules">
+          <a href="<?php print $front_page; ?>?q=node/2" title="Developing a Critical Thinking Attitude">Developing a Critical Thinking Attitude</a>
+        </li>
+        <li class="nav-modules">
+          <a href="<?php print $front_page; ?>?q=node/2" title="Critical Thinking Skills Training">Critical Thinking Skills Training</a>
+        </li>
+        <li class="nav-modules">
+          <a href="<?php print $front_page; ?>?q=node/2" title="Critical Thinking in New Contexts">Critical Thinking in New Contexts</a>
+        </li>
+        <li class="nav-modules">
+          <a href="<?php print $front_page; ?>?q=node/2" title="Metacognition">Metacognition</a>
+        </li>
+        <li id="nav-explore">
+          <a href="<?php print $front_page; ?>" title="Explore">Explore</a>
+        </li>
+      </ul>
+    </nav>
+    
+<?php //end navigation
+?>
